088ecf951c28cfafec647ad3645d6e76ce30af08
Sukhjit <ssukhjit9211@gmail.com>
2023-11-06 11:29:33 -0500

    modified urls and added views.py

--- til/urls.py
+++ til/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from feed import urls as feed_urls
+from django.conf.url import include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include(feed_urls, namespace="feed"))
 ]

c7a4b471f89507a2cd8e7c06ad9a46270fd6d63a
Sukhjit <ssukhjit9211@gmail.com>
2023-11-15 13:26:57 -0500

    added thumbnail and put media in gitignore

--- til/urls.py
+++ til/urls.py
@@ -20,9 +20,11 @@ from django.urls import path
 from feed import urls as feed_urls
 from django.conf.urls import include
 from django.urls import re_path 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include(feed_urls, namespace="feed")),
     re_path(r'^', include("allauth.urls")), 
-]
+] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)

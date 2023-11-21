cf43c3512857fd9428defc509233453266faaee3
Sukhjit <ssukhjit9211@gmail.com>
2023-11-21 14:13:04 -0500

    added modal

--- til/settings.py
+++ til/settings.py
@@ -154,3 +154,8 @@ AUTHENTICATION_BACKENDS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS =[
+    os.path.join(PROJECT_DIR, "frontend/")
+]
+STATIC_ROOT = os.path.join(BASE_DIR,"static")
+STATIC_URL = "/static/"

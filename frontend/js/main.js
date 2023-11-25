2ab9d98cd6154813c07b7c8a91ed26281dfa7862
Sukhjit <ssukhjit9211@gmail.com>
2023-11-25 10:11:30 -0500

    dont forget to check spelling correct the statement misspelled is_authenticated and added jquery in mainjs

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,26 @@
-console.log("hello world")
+$.ajaxSetup({
+    beforeSend: function beforeSend(xhr, settings) {
+      function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+          const cookies = document.cookie.split(';');
+          for (let i = 0; i < cookies.length; i += 1) {
+            const cookie = jQuery.trim(cookies[i]);
+            if (cookie.substring(0, name.length + 1) === (`${name}=`)) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+            }
+          }
+        }
+        return cookieValue;
+      }
+  
+      if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+        xhr.setRequestHeader('X-CSRFToken', getCookie('csrftoken')); // Add CSRF token to request headers
+      }
+    }
+  });
+  $(document).on("click",".js-toggle-modal", function(e){
+    e.preventDefault()
+    $(".js-modal").toggleClass("hidden")
+  })

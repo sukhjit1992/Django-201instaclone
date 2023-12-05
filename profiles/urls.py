64c6e33b795ae42af75d12bd3c61efd333c9a0dc
Sukhjit <ssukhjit9211@gmail.com>
2023-12-05 15:22:04 -0500

    added the follow and unfollow on same button while click on it

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from . import views
 app_name= "profiles"
 urlpatterns=[
     path("<str:username>/", views.ProfileDetailView.as_view(), name="detail"),
+     path("<str:username>/follow/", views.FollowView.as_view(), name="follow")
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from feed.models import Post
+from django.http import JsonResponse
+from follower.models import Follower  
+
 
 class ProfileDetailView(DetailView):
     http_method_name= ["get"]
@@ -16,5 +20,35 @@ class ProfileDetailView(DetailView):
         context['total_follower'] = Post.objects.filter(author=user).count()
         return context
 
+
+class FollowView(LoginRequiredMixin, View ):
+    http_method_names = ["post"]
+    def post(self, request, *args, **kwargs):
+        data = request.POST.dict()
+        if "action" not in data or "username" not in data:
+            return HttpResponseBadRequest("Missinng data")
+        try:
+            other_user= User.objects.get(username =data['username'])
+        except User.DoesNotExist:
+            return HttpResponseBadRequest("Missing user")
+        if data["action"]=="follow":
+            follower,created = Follower.objects.get_or_create(
+                followed_by=request.user,
+                following = other_user
+            )
+        else:
+            try:
+                follower = Follower.objects.get(
+                    followed_by=request.user,
+                    following= other_user,
+                )
+            except Follower.DoesNotExist:
+                follower= None
+            if follower:
+                follower.delete()
+        return JsonResponse({
+            'success': True,
+            'wording':"Unfollow" if data['action']== 'follow' else 'follow'
+        })
 
 
